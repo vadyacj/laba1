249b3147e73626fc87d5945c06cf3a08171bec53
vadyacj <71759435+vadyacj@users.noreply.github.com>
2020-10-20 14:13:49 +0300

    Add files via upload

---
+++ lab4.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <windows.h>
+#include <locale.h>
+
+int main()
+{
+	int j, i, pr = 1, sum = 0, kol = 0;
+	const int m = 2, n = 2;
+	int a[m], b[n];
+	setlocale(LC_ALL, "Russian");
+	printf("Заполните массив\n");
+	for (i = 0; i < n; i++) {
+		scanf_s("%d", &a[i]);
+		scanf_s("%d", &b[i]);
+	}
+
+	for (i = 0; i < n; i++) 
+	{
+		for (j = 0; j < m; j++) {
+			pr = a[i] * b[j];
+			if (pr > 10) {
+				kol++;
+				printf("\na[%d]  b[%d]", i , j);
+				sum += pr;
+			}
+		}
+	}
+	printf("\nsum = %d", sum);
+	printf("\nkol = %d", kol);
+	
+
+	return 0;
+}
