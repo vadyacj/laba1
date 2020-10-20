c1d7fb6d19f32c761bf130c2676fec4db5112202
vadyacj <71759435+vadyacj@users.noreply.github.com>
2020-10-20 13:09:10 +0300

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <windows.h>
+#include <locale.h>
+
+int main()
+{
+	int n = 0;
+	do {
+		n++;
+	} while ((7 * n * n * n + 81 * n * n - 1000000) < 0);
+	printf("%d", n);
+	return 0;
+}
